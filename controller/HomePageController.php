c644b9d0b7abc3ffa8e573806a00515151aa936c
najia01 <validress@outlook.fr>
2023-07-06 16:55:03 +0200

    page home ok

--- controller/HomePageController.php
+++ controller/HomePageController.php
@@ -7,10 +7,17 @@ class HomePageController extends Controller
         $citiesBeach = $model->getCityBeach();
         $cityMountain = $model->getCityMountains();
         $modelImg = new ImageModel;
+        $modelImgDome = new ImageModel;
+        $modelImgPeniche = new ImageModel;
+        $modelImgYourte = new ImageModel;
         $images = $modelImg->getImg();
+        $imagesDome = $modelImgDome->getImgDome();
+        $imagesPeniche = $modelImgPeniche->getImgPeniche();
+        $imagesYourte = $modelImgYourte->getImgYourte();
        
+
         $twig = $this->getTwig();
-        echo $twig->render('homePage.html.twig', ['cityBeach' => $citiesBeach, 'cityMountain' => $cityMountain, 'images'=>$images]);
+        echo $twig->render('homePage.html.twig', ['cityBeach' => $citiesBeach, 'cityMountain' => $cityMountain, 'imagesDome'=>$imagesDome,'imagesPeniche'=>$imagesPeniche,'images'=>$images,'imagesYourte'=>$imagesYourte]);
     }
     // -*-*-*-*METHOD FOOTER LEGAL NOTICE  -*-*-*-*//
     public function legalNotices()
--- model/SearchModel.php
+++ model/SearchModel.php
@@ -5,20 +5,47 @@ class SearchModel extends Model
     public function getSearch($searchResult)
 
     {
-        $logements =[];
+      
+// Récupérer les valeurs du formulaire
+$city = $_POST['city'];
+$start_date = $_POST['start_date'];
+$end_date = $_POST['end_date'];
+$number_of_person = $_POST['number_of_person'];
 
-        $sql=$this->getDb()->query(SELECT * FROM `book`
-        WHERE ville = 'nom_de_la_ville'
-          AND date_arrivee >= 'date_arrivee_recherchee'
-          AND date_depart <= 'date_depart_recherchee'
-          AND nombre_personnes >= 'nombre_personnes_recherche'
-        ' );
-       
-       
-        while ($logement = $sql->fetch(PDO::FETCH_ASSOC)) {
-            $logements[] = new Logement($logement);
-        }
 
-        return $logements;
-    }
+
+// Préparation et exécution de la requête
+$stmt = $this->getDb()->prepare("SELECT logement.*
+FROM logement
+INNER JOIN villes_france ON logement.city = villes_france.ville_nom
+LEFT JOIN book ON logement.id_logement = book.id_logement
+WHERE villes_france.ville_nom = ?
+AND logement.number_of_person >= ?
+AND (
+    (book.start_date IS NULL AND book.end_date IS NULL)
+    OR (book.start_date > ? OR book.end_date < ?)
+    OR (book.start_date > ? AND book.end_date < ?)
+    OR (? > book.start_date AND ? < book.end_date)
+)");
+$stmt->execute([
+    $city,
+    $number_of_person,
+    $end_date,
+    $start_date,
+    $end_date,
+    $start_date,
+    $start_date,
+    $end_date
+]);
+
+// Récupérer les résultats de la requête
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Utilisez les résultats comme vous le souhaitez (par exemple, affichage des résultats)
+foreach ($results as $row) {
+    echo $row['title'] . "<br>";
+    // Afficher les autres informations du logement
+}
+
+}
 }
