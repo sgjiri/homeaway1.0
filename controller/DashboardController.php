630f6c28e6a4add0858a18342047a13b16adacaf
sg.jiri <sg.jiri@yahoo.fr>
2023-08-08 16:52:42 +0200

    ajoutLogement

--- controller/DashboardController.php
+++ controller/DashboardController.php
@@ -7,11 +7,11 @@ class DashboardController extends Controller
         $idUser = $_SESSION['id_person'];
         $model = new DashboardModel();
         $modelUpdate = new DashboardUpdateLogementModel();
-       
+
         if (!$_POST) {
             $datas = $model->getUser($idUser);
             $datasLogement = $modelUpdate->getLogement($idUser);
-            echo $twig->render('templateDashboard.html.twig', ['user' => $datas, 'logement' => $datasLogement ]);
+            echo $twig->render('templateDashboard.html.twig', ['user' => $datas, 'logement' => $datasLogement]);
         }
         if (isset($_POST['validerName'])) {
             $firstname = ($_POST['firstname']);
@@ -20,6 +20,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentInfoPerso");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -30,6 +31,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentInfoPerso");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -40,6 +42,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentInfoPerso");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -49,6 +52,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentInfoPerso");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -62,12 +66,14 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentInfoPerso");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]), "<script>alert(\"changement de mot de passe ruisi\")</script>";
             } else {
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentInfoPerso");
                 echo "<script>alert(\"les donné saisi ne sont pas les meme\")</script>";
             }
         }
-       
+
 
         if (isset($_POST['validerTitle'])) {
             $title = ($_POST['title']);
@@ -76,6 +82,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -83,15 +90,16 @@ class DashboardController extends Controller
         if (isset($_POST['validerAdresse'])) {
             $adresseSaisis = ($_POST['adresse']);
             $idLogement = ($_POST['idLogement']);
-            $tableauAdresse = explode(", ",$adresseSaisis);
+            $tableauAdresse = explode(", ", $adresseSaisis);
             $adresse = $tableauAdresse[0];
             $codePostale = $tableauAdresse[1];
             $city = $tableauAdresse[2];
-            
+
             $return = $modelUpdate->setAdresse($adresse, $codePostale, $city, $idUser, $idLogement);
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -103,6 +111,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -114,6 +123,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -125,6 +135,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -136,6 +147,7 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
@@ -147,9 +159,235 @@ class DashboardController extends Controller
 
             if ($return) {
                 $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
+                echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
+            }
+        }
+        if (isset($_POST['validerBeds'])) {
+            $number_of_beds = ($_POST['number_of_beds']);
+            $idLogement = ($_POST['idLogement']);
+            $return = $modelUpdate->setBeds($number_of_beds, $idUser, $idLogement);
+
+            if ($return) {
+                $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
                 echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
             }
         }
 
+        if (isset($_POST['validerDescription'])) {
+            $description = ($_POST['description']);
+            $idLogement = ($_POST['idLogement']);
+            $return = $modelUpdate->setDescription($description, $idUser, $idLogement);
+
+            if ($return) {
+                $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
+                echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
+            }
+        }
+
+        if (isset($_POST['validerResume'])) {
+            $resume = ($_POST['resume']);
+            $idLogement = ($_POST['idLogement']);
+            $return = $modelUpdate->setResume($resume, $idUser, $idLogement);
+
+            if ($return) {
+                $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
+                echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
+            }
+        }
+
+
+
+        if (isset($_POST['validerEquipment'])) {
+            $parking = isset($_POST['parking']) && $_POST['parking'] == 1 ? true : false;
+            $wifi = isset($_POST['wifi']) &&  $_POST['wifi'] == 1 ? true : false;
+            $piscine = isset($_POST['piscine']) &&  $_POST['piscine'] == 1 ? true : false;
+            $animals = isset($_POST['animals']) && $_POST['animals'] == 1 ? true : false;
+            $kitchen = isset($_POST['kitchen']) && $_POST['kitchen'] == 1 ? true : false;
+            $garden = isset($_POST['garden']) && $_POST['garden'] == 1 ? true : false;
+            $tv = isset($_POST['tv']) && $_POST['tv'] == 1 ? true : false;
+            $climatisation = isset($_POST['climitisation']) && $_POST['climatisation'] == 1 ? true : false;
+            $camera = isset($_POST['camera']) && $_POST['camera'] == 1 ? true : false;
+            $home_textiles = isset($_POST['home_textiles']) && $_POST['home_textiles'] == 1 ? true : false;
+            $spa = isset($_POST['spa']) &&  $_POST['spa'] == 1 ? true : false;
+            $jacuzzi = isset($_POST['jacuzzi']) &&  $_POST['jacuzzi'] == 1 ? true : false;
+            $idLogement = ($_POST['idLogement']);
+            $return = $modelUpdate->setEquipment($parking, $wifi, $piscine, $animals, $kitchen, $garden, $tv, $climatisation, $camera, $home_textiles, $spa, $idUser, $jacuzzi, $idLogement);
+
+            if ($return) {
+                $datas = $model->getUser($idUser);
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentGestionLogement");
+                echo $twig->render('templateDashboard.html.twig', ['user' => $datas]);
+            }
+
+
+           
+
+        }
+        if (isset($_POST['submitAddLogement'])) {
+            // var_dump($_POST);
+
+            if (isset($_SESSION['id_person'])) {
+                // var_dump('id_person is set');
+
+                $id_person = $_SESSION['id_person'];
+                $title = $_POST['title'];
+                $type = $_POST['type'];
+                var_dump($type);
+                $surface = $_POST['surface'];
+                $resume = $_POST['resume'];
+                $description = $_POST['description'];
+                $adress = $_POST['adress'];
+                $adressCode = $_POST['adressCode'];
+                $city = $_POST['city'];
+                $location = $_POST['location'];
+                $price_by_night = $_POST['price_by_night'];
+                $number_of_person = $_POST['number_of_person'];
+                $number_of_beds = $_POST['number_of_beds'];
+                $parking = isset($_POST['parking+']) && $_POST['parking+'] == 1 ? true : false;
+                $wifi = isset($_POST['wifi+']) &&  $_POST['wifi+'] == 1 ? true : false;
+                $piscine = isset($_POST['piscine+']) &&  $_POST['piscine+'] == 1 ? true : false;
+                $animals = isset($_POST['animals+']) && $_POST['animals+'] == 1 ? true : false;
+                $kitchen = isset($_POST['kitchen+']) && $_POST['kitchen+'] == 1 ? true : false;
+                $garden = isset($_POST['garden+']) && $_POST['garden+'] == 1 ? true : false;
+                $tv = isset($_POST['tv+']) && $_POST['tv+'] == 1 ? true : false;
+                $climatisation = isset($_POST['climitisation+']) && $_POST['climatisation+'] == 1 ? true : false;
+                $camera = isset($_POST['camera+']) && $_POST['camera+'] == 1 ? true : false;
+                $home_textiles = isset($_POST['home_textiles+']) && $_POST['home_textiles+'] == 1 ? true : false;
+                $spa = isset($_POST['spa+']) &&  $_POST['spa+'] == 1 ? true : false;
+                $jacuzzi = isset($_POST['jacuzzi+']) &&  $_POST['jacuzzi+'] == 1 ? true : false;
+
+                $adresse = $adress . $adressCode . $city;
+
+                // Préparer l'URL pour l'appel à l'API Google Maps Geocoding
+                $apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($adresse) . '&key=AIzaSyBJOi0dF-SaAfjQDQogCfi0Ns5BDtZcJJU';
+
+                // Effectuer l'appel à l'API
+                $response = file_get_contents($apiUrl);
+
+                // Analyser la réponse JSON
+                $data = json_decode($response, true);
+
+                // Vérifier si la requête s'est bien déroulée
+                if ($data['status'] === 'OK') {
+                    // Récupérer les coordonnées géographiques
+                    $latitude = $data['results'][0]['geometry']['location']['lat'];
+                    $longitude = $data['results'][0]['geometry']['location']['lng'];
+
+                    // Insérer les coordonnées dans la base de données en utilisant le modèle
+                }
+
+                // crée une nouvelle instance de la classe LogementModel et la stocke dans la variable $img
+                $img = new LogementModel();
+
+                // initialise un tableau vide $thumbnailDatas. Ce tableau sera utilisé pour stocker les noms des fichiers des miniatures téléchargées.
+                $thumbnailDatas = [];
+
+                // compte le nombre de fichiers téléchargés dans la superglobale $_FILES et stocke le résultat dans la variable $nb.
+                $nb = count($_FILES);
+                function resizeImg($tmp_name, $width, $height, $name)
+                {
+                    // Récupération des dimensions de l'image d'origine
+                    list($x, $y) = getimagesize($tmp_name);
+
+                    // Calcul du ratio de redimensionnement en fonction des dimensions souhaitées
+                    $ratio = min($width / $x, $height / $y);
+                    $new_width = round($x * $ratio);
+                    $new_height = round($y * $ratio);
+
+                    // Récupération de l'extension du fichier
+                    $ext = pathinfo($name, PATHINFO_EXTENSION);
+
+                    switch ($ext) {
+                        case 'jpg':
+                        case 'jpeg':
+                            $imageCreateFrom = 'imagecreatefromjpeg';
+                            $imageExt = 'imagejpeg';
+                            break;
+                        case 'png':
+                            $imageCreateFrom = 'imagecreatefrompng';
+                            $imageExt = 'imagepng';
+                            break;
+                        case 'gif':
+                            $imageCreateFrom = 'imagecreatefromgif';
+                            $imageExt = 'imagegif';
+                            break;
+                        case 'webp':
+                            $imageCreateFrom = 'imagecreatefromwebp';
+                            $imageExt = 'imagewebp';
+                            break;
+                        default:
+                            throw new Exception("Format d'image non pris en charge");
+                    }
+
+                    // Création d'une nouvelle image avec les nouvelles dimensions 
+                    $image = $imageCreateFrom($tmp_name);
+                    $image_p = imagecreatetruecolor($new_width, $new_height);
+                    imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $x, $y);
+
+                    // Sauvegarde de l'image redimensionnée dans le répertoire
+                    $imageExt($image_p, "./asset/img/logement/" . $name);
+                }
+                // boucle for itère à travers chaque fichier téléchargé en utilisant une variable d'index $i.
+                for ($i = 1; $i <= $nb; $i++) {
+
+                    // : Cette condition vérifie si le fichier téléchargé associé à l'index $i a été téléchargé sans erreur (UPLOAD_ERR_OK). Si c'est le cas, le bloc suivant est exécuté.
+
+                    if ($_FILES['thumbnail_' . $i]['error'] === UPLOAD_ERR_OK) {
+
+                        // Cette condition vérifie si le nom du fichier téléchargé pour l'index $i est défini. 
+
+                        if (isset($_FILES['thumbnail_' . $i]['name'])) {;
+                        }
+
+                        // Cette ligne récupère le chemin temporaire du fichier téléchargé pour l'index $i dans la superglobale $_FILES et le stocke dans la variable $tmp_path.
+                        $tmp_path = $_FILES['thumbnail_' . $i]['tmp_name'];
+
+                        // Cette ligne génère un nouveau nom de fichier en utilisant l'index $i et le nom original du fichier téléchargé, en les concaténant avec le préfixe 'thumbnail_' et un underscore _.
+
+                        $new_filename = 'thumbnail_' . $i . '_' . $_FILES['thumbnail_' . $i]['name'];
+
+                        // Cette ligne génère le nouveau chemin complet où le fichier téléchargé sera déplacé. Dans cet exemple, les fichiers seront stockés dans le répertoire 'C:/wamp64/www/Projet/homeaway1.0/asset/img/' avec le nouveau nom de fichier.
+                        $new_path = './asset/img/logement/' . $new_filename;
+
+                        // Cette ligne déplace le fichier téléchargé du chemin temporaire ($tmp_path) vers le nouveau chemin spécifié ($new_path). Ainsi, le fichier est déplacé du répertoire temporaire du serveur vers le répertoire de destination choisi.
+                        move_uploaded_file($tmp_path, $new_path);
+
+                        // Cette ligne ajoute un nouvel élément au tableau $thumbnailDatas. L'élément est un tableau associatif avec une clé 'thumbnail' et la valeur est le nom du fichier généré pour l'index $i. Ainsi, le tableau $thumbnailDatas contiendra tous les noms des fichiers des miniatures téléchargées.
+
+                        $thumbnailDatas[] = [
+                            'thumbnail' => $new_filename
+                        ];
+                        // le tableau $thumbnailDatas contiendra les noms de tous les fichiers des miniatures téléchargées, et vous pouvez utiliser ce tableau pour les enregistrer dans la base de données ou les traiter d'une autre manière dans le modèle LogementModel.
+
+
+                        // Appel de la fonction resizeImg() avec les paramètres suivants :
+                        // - Le chemin temporaire de l'image dans $_FILES['thumbnail']
+                        // - Les dimensions de largeur et de hauteur souhaitées (300x300 dans ce cas)
+                        // - Le nom de fichier dans $_FILES['thumbnail']
+
+                        // Cette condition vérifie si le fichier thumbnail a été téléchargé avec succès (UPLOAD_ERR_OK). La superglobale $_FILES est utilisée pour vérifier l'existence du fichier et s'assurer qu'il n'y a pas d'erreur lors du téléchargement.
+                        if (isset($_FILES['thumbnail']) && $_FILES['thumbnail']['error'] === UPLOAD_ERR_OK) {
+                            // : Si la condition est vraie, cette ligne appelle la fonction resizeImg() pour redimensionner l'image thumbnail. Les paramètres passés à la fonction sont les suivants :
+                            resizeImg($_FILES['thumbnail']['tmp_name'], 300, 300, $_FILES['thumbnail']['name']);
+                        }
+                    }
+                }
+                $logementmodel = new LogementModel();
+                $idLogement = $logementmodel->addFlat($id_person, $title, $type, $surface, $resume, $description, $adress, $adressCode, $city, $location,  $price_by_night, $number_of_person, $number_of_beds, $parking, $wifi, $piscine, $animals, $kitchen, $garden, $tv, $climatisation, $camera, $home_textiles, $spa, $jacuzzi, $latitude, $longitude);
+                $thumbnailImg = $img->getUpload($thumbnailDatas, $idLogement);
+
+                header("Location: /Projet/homeaway1.0/dashboard?activeElement=contentAjoutLogement");
+            } else {
+                $twig = $this->getTwig();
+                echo $twig->render('templateDashboard.html.twig', []);
+            }
+        }
     }
-}
+
+   
+    }
+
--- model/DashboardUpdateLogementModel.php
+++ model/DashboardUpdateLogementModel.php
@@ -44,40 +44,92 @@ class DashboardUpdateLogementModel extends Model{
     public function setSurface($surface, $idUser, $idLogement)
     {
 
-        $updateType = $this->getDb()->prepare('UPDATE `logement` SET `surface` = :surface WHERE `id_person` = :userId AND `id_logement`= :idLogement');
-        $updateType->bindParam(':surface', $surface, PDO::PARAM_STR);
-        $updateType->bindParam(':userId', $idUser, PDO::PARAM_STR);
-        $updateType->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
-        return $updateType->execute();
+        $updateSurface = $this->getDb()->prepare('UPDATE `logement` SET `surface` = :surface WHERE `id_person` = :userId AND `id_logement`= :idLogement');
+        $updateSurface->bindParam(':surface', $surface, PDO::PARAM_STR);
+        $updateSurface->bindParam(':userId', $idUser, PDO::PARAM_STR);
+        $updateSurface->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
+        return $updateSurface->execute();
     }
 
     public function setLocation($location, $idUser, $idLogement)
     {
 
-        $updateType = $this->getDb()->prepare('UPDATE `logement` SET `location` = :location WHERE `id_person` = :userId AND `id_logement`= :idLogement');
-        $updateType->bindParam(':location', $location, PDO::PARAM_STR);
-        $updateType->bindParam(':userId', $idUser, PDO::PARAM_STR);
-        $updateType->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
-        return $updateType->execute();
+        $updateLocation = $this->getDb()->prepare('UPDATE `logement` SET `location` = :location WHERE `id_person` = :userId AND `id_logement`= :idLogement');
+        $updateLocation->bindParam(':location', $location, PDO::PARAM_STR);
+        $updateLocation->bindParam(':userId', $idUser, PDO::PARAM_STR);
+        $updateLocation->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
+        return $updateLocation->execute();
     }
 
     public function setPrice($price_by_night, $idUser, $idLogement)
     {
 
-        $updateType = $this->getDb()->prepare('UPDATE `logement` SET `price_by_night` = :price_by_night WHERE `id_person` = :userId AND `id_logement`= :idLogement');
-        $updateType->bindParam(':price_by_night', $price_by_night, PDO::PARAM_STR);
-        $updateType->bindParam(':userId', $idUser, PDO::PARAM_STR);
-        $updateType->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
-        return $updateType->execute();
+        $updatePrice= $this->getDb()->prepare('UPDATE `logement` SET `price_by_night` = :price_by_night WHERE `id_person` = :userId AND `id_logement`= :idLogement');
+        $updatePrice->bindParam(':price_by_night', $price_by_night, PDO::PARAM_STR);
+        $updatePrice->bindParam(':userId', $idUser, PDO::PARAM_STR);
+        $updatePrice->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
+        return $updatePrice->execute();
     }
 
     public function setPersone($number_of_person, $idUser, $idLogement)
     {
 
-        $updateType = $this->getDb()->prepare('UPDATE `logement` SET `number_of_person` = :number_of_person WHERE `id_person` = :userId AND `id_logement`= :idLogement');
-        $updateType->bindParam(':number_of_person', $number_of_person, PDO::PARAM_STR);
-        $updateType->bindParam(':userId', $idUser, PDO::PARAM_STR);
-        $updateType->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
-        return $updateType->execute();
+        $updatePersone= $this->getDb()->prepare('UPDATE `logement` SET `number_of_person` = :number_of_person WHERE `id_person` = :userId AND `id_logement`= :idLogement');
+        $updatePersone->bindParam(':number_of_person', $number_of_person, PDO::PARAM_STR);
+        $updatePersone->bindParam(':userId', $idUser, PDO::PARAM_STR);
+        $updatePersone->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
+        return $updatePersone->execute();
+    }
+
+    public function setBeds($number_of_beds, $idUser, $idLogement)
+    {
+
+        $updateBeds = $this->getDb()->prepare('UPDATE `logement` SET `number_of_beds` = :number_of_beds WHERE `id_person` = :userId AND `id_logement`= :idLogement');
+        $updateBeds->bindParam(':number_of_beds', $number_of_beds, PDO::PARAM_STR);
+        $updateBeds->bindParam(':userId', $idUser, PDO::PARAM_STR);
+        $updateBeds->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
+        return $updateBeds->execute();
+    }
+
+
+    public function setDescription($description, $idUser, $idLogement)
+    {
+
+        $updateDescription = $this->getDb()->prepare('UPDATE `logement` SET `description` = :description WHERE `id_person` = :userId AND `id_logement`= :idLogement');
+        $updateDescription->bindParam(':description', $description, PDO::PARAM_STR);
+        $updateDescription->bindParam(':userId', $idUser, PDO::PARAM_STR);
+        $updateDescription->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
+        return $updateDescription->execute();
+    }
+
+    public function setResume($resume, $idUser, $idLogement)
+    {
+
+        $updateResumer = $this->getDb()->prepare('UPDATE `logement` SET `resume` = :resume WHERE `id_person` = :userId AND `id_logement`= :idLogement');
+        $updateResumer->bindParam(':resume', $resume, PDO::PARAM_STR);
+        $updateResumer->bindParam(':userId', $idUser, PDO::PARAM_STR);
+        $updateResumer->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
+        return $updateResumer->execute();
+    }
+
+    public function setEquipment($parking, $wifi, $piscine, $animals, $kitchen, $garden, $tv, $climatisation, $camera, $home_textiles, $spa, $idUser, $jacuzzi, $idLogement)
+    {
+
+        $updateEquipment = $this->getDb()->prepare('UPDATE `logement` SET `parking` = :parking, `wifi` = :wifi, `piscine` = :piscine, `animals` = :animals, `kitchen` = :kitchen, `garden` = :garden, `tv` = :tv, `climatisation` = :climatisation, `camera` = :camera, `home_textiles` = :home_textiles, `spa` = :spa, `jacuzzi` = :jacuzzi WHERE `id_person` = :userId AND `id_logement`= :idLogement');
+        $updateEquipment->bindParam(":parking", $parking, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":wifi", $wifi, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":piscine", $piscine, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":animals", $animals, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":kitchen", $kitchen, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":garden", $garden, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":tv", $tv, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":climatisation", $climatisation, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":camera", $camera, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":home_textiles", $home_textiles, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":spa", $spa, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(":jacuzzi", $jacuzzi, PDO::PARAM_BOOL);
+        $updateEquipment->bindParam(':userId', $idUser, PDO::PARAM_STR);
+        $updateEquipment->bindParam(':idLogement', $idLogement, PDO::PARAM_STR);
+        return $updateEquipment->execute();
     }
 }
