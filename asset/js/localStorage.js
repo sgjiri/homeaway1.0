77ad7d3c8ca57e07760d62f35f1ba427595f6c45
najia01 <validress@outlook.fr>
2023-08-09 16:58:14 +0200

    filtre non ok

--- asset/js/localStorage.js
+++ asset/js/localStorage.js
@@ -1,61 +1,58 @@
-
-
 if (document.getElementsByClassName('article')) {
-    const searchForm = document.getElementsByClassName('article');
-    console.log(searchForm);
-
+    const searchForm = document.getElementsByClassName('item');
+  
     for (let i = 0; i < searchForm.length; i++) {
-        searchForm[i].addEventListener('click', (event) => {
-            // Empêcher la soumission du formulaire pour pouvoir manipuler les données
-            event.preventDefault();
-
-            // Récupérer les valeurs des champs de formulaire
-            const city = document.getElementById('cityM').value;
-            const startDate = document.getElementById('start_dateM').value;
-            const endDate = document.getElementById('end_dateM').value;
-            const numberOfPerson = document.getElementById('number_of_personM').value;
-            const idLogement = document.getElementsByClassName('idLogement')[i].innerHTML;
-            const priceByNight = document.getElementsByClassName('priceByNight')[i].innerHTML;
-            
-
-            // Stocker les valeurs dans des cookies
-            document.cookie = `city=${city}`;
-            document.cookie = `start_date=${startDate}`;
-            document.cookie = `end_date=${endDate}`;
-            document.cookie = `number_of_person=${numberOfPerson}`;
-            document.cookie = `priceByNight=${priceByNight}`;
-            console.log(document.cookie);
-            // // Rediriger vers la page oneLogement
-            window.location.href = "http://localhost/Projet/homeaway1.0/one"+idLogement;
-        });
+      searchForm[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        const city = document.getElementById('cityM').value
+        const startDate = document.getElementById('start_dateM').value;
+        const endDate = document.getElementById('end_dateM').value;
+        const numberOfPerson = document.getElementById('number_of_personM').value;
+        const idLogement = document.getElementsByClassName('idLogement')[i].innerHTML;
+        const priceByNight = document.getElementsByClassName('priceByNight')[i].innerHTML;
+        const totalPrices = document.getElementsByClassName('totalPrices')[i].innerHTML;
+  
+        // Stocker les valeurs dans des cookies avec une durée d'expiration plus longue
+        document.cookie = `city=${city}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
+        document.cookie = `start_date=${startDate}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
+        document.cookie = `end_date=${endDate}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
+        document.cookie = `number_of_person=${numberOfPerson}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
+        document.cookie = `priceByNight=${priceByNight}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
+        document.cookie = `totalPrices=${totalPrices}; expires=Fri, 31 Dec 9999 23:59:59 UTC`;
+  
+        console.log(document.cookie);
+  
+        window.location.href = "http://localhost/Projet/homeaway1.0/one" + idLogement;
+      });
     }
+  }
+  
+  const startDate = document.getElementById('startDate');
+  const endDate = document.getElementById('endDate');
+  const numberOfPerson = document.getElementById('numberOfPerson');
+  const priceByNight = document.getElementById('priceByNight');
+  const totalPrices = document.getElementById('totalPrices');
+  const cityHidden = document.getElementById('cityHidden');
+  const startDateHidden = document.getElementById('startDateHidden');
+  const endDateHidden = document.getElementById('endDateHidden');
+  const numberOfPersonHidden = document.getElementById('numberOfPersonHidden');
+  
+  const cookies = document.cookie.split(';');
 
-}
-
-
-
-const startDate = document.getElementById('startDate');
-const endDate = document.getElementById('endDate');
-const numberOfPerson = document.getElementById('numberOfPerson');
-const priceByNight = document.getElementById('priceByNight');
-
-// Récupérer les valeurs des cookies
-const cookies = document.cookie.split(';');
-const cityCookie = cookies.find(cookie => cookie.includes('city='));
-const startDateCookie = cookies.find(cookie => cookie.includes('start_date='));
-const endDateCookie = cookies.find(cookie => cookie.includes('end_date='));
-const numberOfPersonCookie = cookies.find(cookie => cookie.includes('number_of_person='));
-const priceByNightCookie = cookies.find(cookie => cookie.includes('priceByNight='));
-
-// Afficher les valeurs dans le formulaire
-startDate.innerHTML = startDateCookie.split('=')[1];
-endDate.innerHTML = endDateCookie.split('=')[1];
-numberOfPerson.innerHTML = numberOfPersonCookie.split('=')[1];
-priceByNight.innerHTML = priceByNightCookie.split('=')[1];
-
-// Supprimer les cookies après utilisation
-document.cookie = 'city=; expires=Thu, 01 Jan 2033 00:00:00 UTC;';
-document.cookie = 'start_date=; expires=Thu, 01 Jan 2033 00:00:00 UTC;';
-document.cookie = 'end_date=; expires=Thu, 01 Jan 2033 00:00:00 UTC;';
-document.cookie = 'number_of_person=; expires=Thu, 01 Jan 2033 00:00:00 UTC;';
-document.cookie = 'priceByNight=; expires=Thu, 01 Jan 2033 00:00:00 UTC;';
+  const cityCookie = cookies.find(cookie => cookie.includes('city='));
+  const startDateCookie = cookies.find(cookie => cookie.includes('start_date='));
+  const endDateCookie = cookies.find(cookie => cookie.includes('end_date='));
+  const numberOfPersonCookie = cookies.find(cookie => cookie.includes('number_of_person='));
+  const priceByNightCookie = cookies.find(cookie => cookie.includes('priceByNight='));
+  const totalPricesCookie = cookies.find(cookie => cookie.includes('totalPrices='));
+  
+  cityHidden.innerHTML = cityCookie ? cityCookie.split('=')[1].trim() : '';
+  startDate.innerHTML = startDateCookie ? startDateCookie.split('=')[1].trim() : '';
+  startDateHidden.innerHTML = startDateCookie ? startDateCookie.split('=')[1].trim() : '';
+  endDate.innerHTML = endDateCookie ? endDateCookie.split('=')[1].trim() : '';
+  endDateHidden.innerHTML = endDateCookie ? endDateCookie.split('=')[1].trim() : '';
+  numberOfPerson.innerHTML = numberOfPersonCookie ? numberOfPersonCookie.split('=')[1].trim() : '';
+  numberOfPersonHidden.innerHTML = numberOfPersonCookie ? numberOfPersonCookie.split('=')[1].trim() : '';
+  priceByNight.innerHTML = priceByNightCookie ? priceByNightCookie.split('=')[1].trim() : '';
+  totalPrices.innerHTML = totalPricesCookie ? totalPricesCookie.split('=')[1].trim() : '';
+  
--- model/SearchModel.php
+++ model/SearchModel.php
@@ -3,91 +3,43 @@
 class SearchModel extends Model
 {
 
-    public function getSearch($city, $start_date, $end_date, $number_of_person, $selectedFilters)
+    public function getSearch($city, $start_date, $end_date, $number_of_person)
     {
         $logements = [];
-        $filterConditions = [];
-        $filterParams = [];
-      
-        foreach ($selectedFilters as $filter) {
-            $filterConditions[] = "(`logement`.`$filter` = 1)";
-            $filterParams[":$filter"] = 1;
-        }
 
-        $sqlFilters = '';
-        if (!empty($filterConditions)) {
-            $sqlFilters = ' AND (' . implode(' OR ', $filterConditions) . ')';
-        }
-
-
-        $stmt = $this->getDb()->prepare("SELECT DISTINCT  *
-       
-    FROM `logement`
-   
-    LEFT JOIN `book` ON `logement`.id_logement = `book`.logement_id
-    WHERE `logement`.city = :city
-    AND `logement`.number_of_person >= :number_of_person
-    AND (
-        (`book`.`start_date` IS NULL AND `book`.`end_date` IS NULL)
-        OR (`book`.`start_date` > :end_date OR `book`.`end_date` < :start_date)
-        OR (`book`.`start_date` > :start_date AND `book`.`end_date` < :end_date)
-        OR (:start_date > `book`.`start_date` AND :end_date < `book`.`end_date`)
-          -- Condition pour vérifier les réservations sur 5 jours
-        
-    OR (
-        -- Réservation sur 5 jours
-       
-        (:start_date = CURDATE() AND `book`.`start_date` = CURDATE() AND `book`.`end_date` = DATE_ADD(CURDATE(), INTERVAL 4 DAY))
-        -- Réservation couvrant complètement l'intervalle de 5 jours
-        OR (`book`.`start_date` <= :start_date AND `book`.`end_date` >= DATE_ADD(:start_date, INTERVAL 4 DAY))
-
-        AND (`logement`.`wifi` = :wifi OR :wifi = 0)
-
-        AND (`logement`.`parking` = :parking OR :parking = 0)
-
-        AND (`logement`.`kitchen` = :kitchen OR :kitchen = 0)
-
-        AND (`logement`.`garden` = :garden OR :garden = 0)
-        AND (`logement`.`piscine` = :piscine OR :piscine = 0)
-        AND (`logement`.`animals` = :animals OR :animals = 0)
-        AND (`logement`.`tv` = :tv OR :tv = 0)
-        AND (`logement`.`climatisation` = :climatisation OR :climatisation = 0)
-        AND (`logement`.`camera` = :camera OR :camera = 0)
-        AND (`logement`.`home_textiles` = :home_textiles OR :home_textiles = 0)
-        AND (`logement`.`spa` = :spa OR :spa = 0)
-        AND (`logement`.`jacuzzi` = :jacuzzi OR :jacuzzi = 0)
-        
-    )
-    )$sqlFilters");
+        $stmt = $this->getDb()->prepare("
+            SELECT DISTINCT *
+            FROM `logement`
+            LEFT JOIN `book` ON `logement`.id_logement = `book`.logement_id
+            WHERE `logement`.city = :city
+            AND `logement`.number_of_person >= :number_of_person
+            AND (
+                (`book`.`start_date` IS NULL AND `book`.`end_date` IS NULL)
+                OR (`book`.`start_date` > :end_date OR `book`.`end_date` < :start_date)
+                OR (`book`.`start_date` > :start_date AND `book`.`end_date` < :end_date)
+                OR (:start_date > `book`.`start_date` AND :end_date < `book`.`end_date`)
+                OR (
+                    (:start_date = CURDATE() AND `book`.`start_date` = CURDATE() AND `book`.`end_date` = DATE_ADD(CURDATE(), INTERVAL 4 DAY))
+                    OR (`book`.`start_date` <= :start_date AND `book`.`end_date` >= DATE_ADD(:start_date, INTERVAL 4 DAY))
+                )
+            )
+        ");
 
         $stmt->bindParam(':city', $city, PDO::PARAM_STR);
         $stmt->bindParam(':number_of_person', $number_of_person, PDO::PARAM_INT);
-
-        if (!empty($start_date)) {
-            $stmt->bindParam(':start_date', $start_date, PDO::PARAM_STR);
-            $stmt->bindParam(':end_date', $end_date, PDO::PARAM_STR);
-        } else {
-            $today = date("Y-m-d");
-            $stmt->bindParam(':start_date', $today, PDO::PARAM_STR);
-            $stmt->bindParam(':end_date', $today, PDO::PARAM_STR);
-        }
-
-       
-        // $stmt->bindParam(':start_date', $start_date, PDO::PARAM_STR);
-        // $stmt->bindParam(':end_date', $end_date, PDO::PARAM_STR);
+        $stmt->bindParam(':start_date', $start_date, PDO::PARAM_STR);
+        $stmt->bindParam(':end_date', $end_date, PDO::PARAM_STR);
 
         $stmt->execute();
-
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // var_dump($city);
-        $length = (count($results));
-        $newResult = [];
+
+        $length = count($results);
 
         foreach ($results as $logement) {
-            $stmt2 = $this->getDb()->prepare("SELECT * FROM `image` WHERE `id_logement` = :logementId ");
+            $stmt2 = $this->getDb()->prepare("SELECT * FROM `image` WHERE `id_logement` = :logementId");
             $stmt2->bindParam(':logementId', $logement['id_logement'], PDO::PARAM_STR);
             $stmt2->execute();
-            $images =  $stmt2->fetchAll(PDO::FETCH_ASSOC);
+            $images = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 
             $logement['thumbnails'] = $images;
             $logements[] = $logement;
@@ -95,6 +47,88 @@ class SearchModel extends Model
 
         return $logements;
     }
+
+
+    public function searchAccommodations($wifi, $parking, $piscine, $animals, $kitchen, $garden, $tv, $climatisation, $camera, $home_textiles, $spa, $jacuzzi)
+{
+    $reqFilter = "
+        SELECT *
+        FROM logement
+        WHERE (wifi = :wifi
+              AND parking = :parking
+              AND  piscine = :piscine
+              AND  animals = :animals
+              AND  kitchen = :kitchen
+              AND  garden = :garden
+              AND  tv = :tv
+              AND  climatisation = :climatisation
+              AND  camera = :camera
+              AND  home_textiles = :home_textiles
+              AND  spa = :spa
+              AND  jacuzzi = :jacuzzi)";
+
+    $stmt = $this->getDb()->prepare($reqFilter);
+
+    $stmt->bindParam(':wifi', $wifi, PDO::PARAM_BOOL);
+    $stmt->bindParam(':parking', $parking, PDO::PARAM_BOOL);
+    $stmt->bindParam(':piscine', $piscine, PDO::PARAM_BOOL);
+    $stmt->bindParam(':animals', $animals, PDO::PARAM_BOOL);
+    $stmt->bindParam(':kitchen', $kitchen, PDO::PARAM_BOOL);
+    $stmt->bindParam(':garden', $garden, PDO::PARAM_BOOL);
+    $stmt->bindParam(':tv', $tv, PDO::PARAM_BOOL);
+    $stmt->bindParam(':climatisation', $climatisation, PDO::PARAM_BOOL);
+    $stmt->bindParam(':camera', $camera, PDO::PARAM_BOOL);
+    $stmt->bindParam(':home_textiles', $home_textiles, PDO::PARAM_BOOL);
+    $stmt->bindParam(':spa', $spa, PDO::PARAM_BOOL);
+    $stmt->bindParam(':jacuzzi', $jacuzzi, PDO::PARAM_BOOL);
+
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $results;
+}
+
+        //"SELECT * FROM logement WHERE city = :city AND number_of_person >= :numberOfPersons";
+
+        // if ($wifi) {
+        //     $reqFilter .= " OR wifi = 1";
+        // }
+        // if ($parking) {
+        //     $reqFilter .= " OR  parking = 1";
+        // }
+        // if ($piscine) {
+        //     $reqFilter .= " OR  piscine = 1";
+        // }
+        // if ($animals) {
+        //     $reqFilter .= " OR  animals = 1";
+        // }
+        // if ($kitchen) {
+        //     $reqFilter .= " OR  kitchen = 1";
+        // }
+        // if ($garden) {
+        //     $reqFilter .= " OR  garden = 1";
+        // }
+        // if ($tv) {
+        //     $reqFilter .= " OR  tv = 1";
+        // }
+        // if ($climatisation) {
+        //     $reqFilter .= " OR  climatisation = 1";
+        // }
+        // if ($camera) {
+        //     $reqFilter .= " OR  camera = 1";
+        // }
+        // if ($home_textiles) {
+        //     $reqFilter .= " OR  home_textiles = 1";
+        // }
+        // if ($spa) {
+        //     $reqFilter .= " OR  spa = 1";
+        // }
+        // if ($jacuzzi) {
+        //     $reqFilter .= " OR  jacuzzi = 1";
+        // }
+
+       
+
 
 
     public function getLogementsByVille($city, $number_of_person)
