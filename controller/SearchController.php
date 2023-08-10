f77f4ebe26728c86bda90e29bc80d506d079ba57
najia01 <validress@outlook.fr>
2023-08-10 16:50:14 +0200

    filtre okokok

--- controller/SearchController.php
+++ controller/SearchController.php
@@ -51,103 +51,68 @@ class SearchController extends Controller
         }
     }
 
-    // public function applyFilters()
-    // {
-    //     if (isset($_POST['filters'])) {
-    //         $city = isset($_POST['city']) ? ucfirst($_POST['city']) : '';
-    //         $start_date = isset($_POST['start_date']) ? $_POST['start_date'] : '';
-    //         $end_date = isset($_POST['end_date']) ? $_POST['end_date'] : '';
-    //         $number_of_person = isset($_POST['number_of_person']) ? $_POST['number_of_person'] : '';
 
 
-    //         $filtresSelectionnes = $_POST['filters'];
 
-    //         $formValue = [
-    //             'city' => $city,
-    //             'start' => $start_date,
-    //             'end' => $end_date,
-    //             'person' => $number_of_person
-    //         ];
-
-    //         $model = new SearchModel;
-    //         $datas = $model->getSearch($city, $start_date, $end_date, $number_of_person, $filtresSelectionnes);
-
-    //         $twig = $this->getTwig();
-
-    //         $resultSearchView = $twig->render('resultsearch.html.twig', [
-    //             'logementDispo' => $datas,
-    //             'formValue' => $formValue
-    //         ]);
-    //         echo $resultSearchView;
-
-    //         if (empty($datas)) {
-    //             $resultSearchView .= "Aucun logement ne correspond à votre recherche.";
-    //         }
-
-    //         return $resultSearchView;
-    //     }
-    // }
-
-
-   
     public function applyFilter()
-    {
-
+    {  
+        
         if (isset($_POST['city'])) {
             $city = $_POST['city'];
-        } else {
-            $city = '';
         }
+        
 
         if (isset($_POST['start_date'])) {
-            $startDate = $_POST['start_date'];
-        } else {
-            $startDate = '';
+            $start_date = $_POST['start_date'];
         }
+        
 
         if (isset($_POST['end_date'])) {
-            $endDate = $_POST['end_date'];
-        } else {
-            $endDate = '';
+            $end_date = $_POST['end_date'];
         }
+       
 
-        if (isset($_POST['number_of_persons'])) {
-            $numberOfPersons = $_POST['number_of_persons'];
-        } else {
-            $numberOfPersons = 1; // Valeur par défaut si non spécifiée
+        if (isset($_POST['number_of_person'])) {
+            $number_of_person = $_POST['number_of_person'];
         }
+       
+
+        $formValue = [];
+        $formValue['city'] = $city;
+        $formValue['start'] = $start_date;
+        $formValue['end'] = $end_date;
+        $formValue['person'] = $number_of_person;
+
+      
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-
-
-            $wifi = isset($_POST['wifi']) ? 1 : 0;
-            $parking = isset($_POST['parking']) ? 1 : 0;
-            $piscine = isset($_POST['piscine']) ? 1 : 0;
-            $animals = isset($_POST['animals']) ? 1 : 0;
-            $kitchen = isset($_POST['kitchen']) ? 1 : 0;
-            $garden = isset($_POST['garden']) ? 1 : 0;
-            $tv = isset($_POST['tv']) ? 1 : 0;
-            $climatisation = isset($_POST['climatisation']) ? 1 : 0;
-            $camera = isset($_POST['camera']) ? 1 : 0;
-            $home_textiles = isset($_POST['home_textiles']) ? 1 : 0;
-            $spa = isset($_POST['spa']) ? 1 : 0;
-            $jacuzzi = isset($_POST['jacuzzi']) ? 1 : 0;
-
-            var_dump($_POST);
+            $arrayFilter = [];
+            foreach($_POST['filters'] as $value){
+                $arrayFilter[$value] = 1;
+            }
+            
+            var_dump($arrayFilter);
             $modelFilter = new SearchModel;
-            $results = $modelFilter->searchAccommodations($city, $startDate, $endDate, $numberOfPersons, $wifi, $parking, $piscine, $animals, $kitchen, $garden, $tv, $climatisation, $camera, $home_textiles, $spa, $jacuzzi);
-
+            $datas=$modelFilter->searchAccommodations($city, $start_date, $end_date, $number_of_person, $arrayFilter);
+            $totalPrices = [];
+            foreach ($datas as $logementComplet) {
+    
+                $price_by_night = $logementComplet['price_by_night'];
+                $start = new DateTime($start_date);
+                $end = new DateTime($end_date);
+                $number_of_days = $start->diff($end)->days;
+                array_push($totalPrices, $price_by_night * $number_of_days);
+            }
 
             $twig = $this->getTwig();
 
-            $results = $twig->render('resultsearch.html.twig', []);
-            echo $results;
+            $resultsView = $twig->render('filterView.html.twig', ['logementDispo'=>$datas, 'formValue' => $formValue, 'totalPrices' => $totalPrices]);
+            echo $resultsView;
         } else {
             $twig = $this->getTwig();
             $message = 'Aucun logement ne correspond à votre recheche.';
 
-            $results = $twig->render('resultsearch.html.twig', ['message' => $message]);
+            $resultsView = $twig->render('filterView.html.twig', ['message' => $message]);
         }
     }
 
