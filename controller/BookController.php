51b6137adb86e98ec241c2c51f46818a7a42e2ef
najia01 <validress@outlook.fr>
2023-08-18 09:46:58 +0200

    correction part 1

--- controller/BookController.php
+++ controller/BookController.php
@@ -2,22 +2,7 @@
 class BookController extends Controller
 {
 
-    // public function getCompletCalendar()
-    // {
-    //     $model = new BookModel();
-    //     $id_logment = $_POST['id_logment'];
-
-    //     if ($_GET['action'] == 'getReservations' && isset($_GET['id_logment'])) {
-    //         $book = $model->getBookHold($id_logment)($_GET['id_logment']);
-    //         header('Content-Type: application/json');
-    //         echo json_encode($book);
-    //     } else {
-    //         // Afficher la vue par défaut
-    //         $twig = $this->getTwig();
-    //         echo $twig->render('oneLogement.html.twig', []);
-    //     }
-    // }
-
+    
     public function getReservation()
 
     {
