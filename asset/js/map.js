5673522a5d5491a16c2de410661b309feab70877
Sequeira Alexandre <67326944+bobas01@users.noreply.github.com>
2023-08-20 12:47:55 +0200

    map fixed

--- asset/js/map.js
+++ asset/js/map.js
@@ -1,45 +1,39 @@
-// Récupérer l'élément de la carte dans le DOM
+// Récupérer l'élément de la carte
 let mappy = document.getElementById('map');
 
 // Classe LeafletMap pour gérer la carte Leaflet
 class LeafletMap {
   constructor() {
-    this.map = null; // Référence à l'objet de la carte Leaflet
-    this.bounds = []; // Tableau des points des marqueurs pour ajuster le zoom de la carte
-    this.activeMarker = null; // Référence au marqueur actif
+    this.map = null;
+    this.bounds = [];
+    this.activeMarker = null;
   }
 
-  // Méthode pour charger la carte Leaflet
   async load(element) {
     return new Promise((resolve, reject) => {
-      // Charger la bibliothèque Leaflet depuis le CDN Content Delivery Network (Réseau de diffusion de contenu). $scrit est une fonction d'une bibliothèque tierce appelée "script.js" qui facilite le chargement asynchrone de scripts externes.
+      // Charger la bibliothèque Leaflet
       $script("https://unpkg.com/leaflet@1.9.4/dist/leaflet.js", () => {
-        // Créer la carte Leaflet dans l'élément spécifié
+        // Créer la carte Leaflet
         this.map = L.map(element, { scrollWheelZoom: false });
-        
-        // Ajouter la couche de tuiles de la carte à partir de Stadia Maps
+        // Ajouter la couche de tuiles de la carte
         L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
           attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         }).addTo(this.map);
-        
-        resolve(); // Résoudre la promesse une fois que la carte est chargée
+        resolve();
       });
     });
   }
 
-  // Méthode pour ajouter un marqueur à la carte
   addMarker(lat, lng, text) {
     let point = [lat, lng];
-    this.bounds.push(point); // Ajouter le point du marqueur aux limites de la carte
+    this.bounds.push(point);
     return new LeafletMarker(point, text, this.map, this);
   }
 
-  // Méthode pour centrer la carte sur tous les marqueurs
   center() {
     this.map.fitBounds(this.bounds);
   }
 
-  // Méthode pour définir le marqueur actif
   setActiveMarker(marker) {
     if (this.activeMarker !== null && this.activeMarker !== marker) {
       this.activeMarker.resetContent();
@@ -47,7 +41,6 @@ class LeafletMap {
     this.activeMarker = marker;
   }
 
-  // Méthode pour réinitialiser le marqueur actif
   resetActiveMarker() {
     if (this.activeMarker !== null) {
       this.activeMarker.resetContent();
@@ -75,30 +68,25 @@ class LeafletMarker {
     this.leafletMap = leafletMap;
   }
 
-  // Méthode pour définir le marqueur comme actif
   setActive() {
     this.popup.getElement().classList.add('is-active');
   }
 
-  // Méthode pour définir le marqueur comme inactif
   unsetActive() {
     this.popup.getElement().classList.remove('is-active');
   }
 
-  // Méthode pour ajouter un écouteur d'événement au marqueur
   addEventListener(event, callback) {
     this.popup.addEventListener('add', () => {
       this.popup.getElement().addEventListener(event, callback);
     });
   }
 
-  // Méthode pour définir le contenu du marqueur
   setContent(text) {
     this.popup.setContent('<div class="popup-content">' + text + '</div>');
     this.popup.getElement().classList.add('is-expanded');
     this.popup.update();
 
-    // Ajouter un écouteur d'événement au bouton de fermeture du marqueur
     const closeButton = this.popup.getElement().querySelector('.close-button');
     closeButton.addEventListener('click', (event) => {
       event.stopPropagation();
@@ -107,7 +95,6 @@ class LeafletMarker {
     });
   }
 
-  // Méthode pour réinitialiser le contenu du marqueur
   resetContent() {
     this.popup.setContent('<div class="popup-content">' + this.text + '</div>');
     this.popup.getElement().classList.remove('is-expanded');
@@ -115,32 +102,42 @@ class LeafletMarker {
   }
 }
 
-// Fonction d'initialisation de la carte et des marqueurs
+// Fonction d'initialisation de la carte
 const initMap = async function () {
-  try {
-    const leafletMap = new LeafletMap(); // Créer une instance de LeafletMap
-
-    await leafletMap.load(mappy); // Charger la carte Leaflet
-
-    // Récupérer tous les marqueurs à partir des éléments avec la classe "js-marker"
-    const markers = document.querySelectorAll('.js-marker');
-
-    markers.forEach((marker) => {
-      const lat = marker.getAttribute('data-lat');
-      const lng = marker.getAttribute('data-lng');
-      const text = marker.innerHTML;
-
-      // Ajouter un marqueur à la carte
-      leafletMap.addMarker(lat, lng, text);
+  let map = new LeafletMap();
+  let hoverMarker = null;
+  await map.load(mappy);
+  // Récupérer tous les éléments de marqueur
+  Array.from(document.querySelectorAll('.js-marker')).forEach((item) => {
+    let marker = map.addMarker(item.dataset.lat, item.dataset.lng, item.dataset.price + '€');
+    item.addEventListener('mouseover', function () {
+      if (hoverMarker !== null) {
+        hoverMarker.unsetActive();
+      }
+      marker.setActive();
+      hoverMarker = marker;
     });
-
-    leafletMap.center(); // Centrer la carte sur tous les marqueurs
-  } catch (error) {
-    console.error(error);
-  }
+    item.addEventListener('mouseleave', function () {
+      if (hoverMarker !== null) {
+        hoverMarker.unsetActive();
+      }
+    });
+    marker.addEventListener('click', function () {
+      if (map.activeMarker === marker && marker.popup.getElement().classList.contains('is-expanded')) {
+        marker.resetContent();
+        marker.unsetActive();
+        map.resetActiveMarker();
+      } else {
+        marker.setContent(item.innerHTML);
+        map.setActiveMarker(marker);
+      }
+    });
+  });
+  map.center();
 };
 
-// Vérifier si l'élément de la carte existe dans le DOM et l'initialiser
+// Vérifier si l'élément de la carte existe
 if (mappy !== null) {
+  // Initialiser la carte
   initMap();
 }
